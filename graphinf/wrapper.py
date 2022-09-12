7a8ad832806d1c601bc20be6082a976ac501f833
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2022-09-12 10:37:14 -0400

    adding wrapped method to __dict__ in wrapper

--- graphinf/wrapper.py
+++ graphinf/wrapper.py
@@ -5,11 +5,19 @@ __all__ = ("Wrapper",)
 
 @dataclass
 class Wrapper:
-    def __init__(self, wrapped, setup_func=None, **others):
+    def __init__(self, wrapped, setup_func=None, omitted_member=None, **others):
         self.__wrapped__ = wrapped
         self.__others__ = others
+        omitted_member = [] if omitted_member is None else omitted_member
         if setup_func is not None:
             setup_func(self.__wrapped__, self.__others__)
+        for m in dir(self.__wrapped__):
+            if (
+                m[:2] != "__"
+                and m not in dir(self.__class__)
+                and m not in omitted_member
+            ):
+                self.__dict__[m] = getattr(self.__wrapped__, m)
 
     @property
     def wrap(self):
@@ -28,8 +36,6 @@ class Wrapper:
     def __getattr__(self, key):
         if key in self.__dict__:
             return getattr(self, key)
-        elif hasattr(self.__wrapped__, key):
-            return getattr(self.__wrapped__, key)
         elif key in self.__others__:
             return self.__others__[key]
         raise AttributeError(
